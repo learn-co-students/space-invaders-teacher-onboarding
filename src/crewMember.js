9c2b9fc92061118ba9710d78572ec6b282c184b3
Edward Sparks <edd_sparks@hotmail.com>
2018-10-10 12:36:03 +0100

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,26 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip = "Looking for a Rig") {
+    this.position = position
+    this.currentShip = currentShip
+  }
+
+  check() {
+   return this.currentShip === "Looking for a Rig"
+  }
+
+  engageWarpDrive() {
+    return (this.check() || this.position !== 'Pilot' ? "had no effect" :
+      this.currentShip.warpDrive = "engaged")
+  }
+
+  setsInvisibility() {
+    return (this.check() || this.position !== 'Defender' ? "had no effect" :
+      this.currentShip.cloaked = true)
+  }
+
+  chargePhasers() {
+    return (this.check() || this.position !== 'Gunner' ? "had no effect" :
+     this.currentShip.phasersCharge = "charged")
+  }
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,26 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged", phasersCharge = 'uncharged'){
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = cloaked
+    this.warpDrive = warpDrive
+    this.phasersCharge = phasersCharge
+    this.crewCheck()
+  }
+
+  crewCheck() {
+
+    if(this.crew.length === 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+      this.crew.forEach(crew => {
+        crew.currentShip = this
+      })
+    }
+  }
+}
+
+
