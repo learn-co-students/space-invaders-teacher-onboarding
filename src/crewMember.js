c25349f1763362bb2e5d9d8094e14b125cb9e4bf
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-11-26 15:21:52 +0000

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,29 @@
-class CrewMember {}
+class CrewMember {
+    constructor(position) {
+      this.position = position
+      this.currentShip = "Looking for a rig"
+    }
+
+    engageWarpDrive() {
+      if (typeof(this.currentShip) == "object" && this.position == "Pilot") {
+        this.currentShip.warpDrive = "engaged";
+      } else {
+        return "had no effect"
+      }
+    }
+
+    setsInvisibility() {
+      return typeof(this.currentShip) == "object" && this.position == "Defender")
+      ? this.currentShip.cloaked = true;
+      : return "had no effect"
+    }
+
+    chargePhasers() {
+      return typeof(this.currentShip) == "object" && this.position == "Gunner" 
+      ? this.currentShip.phasersCharge = "charged";
+      : return "had no effect"
+      }
+    }
+}
+
+// export default CrewMember;
